e4bc8cd2d64e1da8245d16ad235d7f46ae139e79
Agus Hermanto <22agushermanto@gmail.com>
2023-10-12 16:05:32 +0700

    tiga commit

--- api/middlewares/middlewares.go
+++ api/middlewares/middlewares.go
@@ -4,8 +4,8 @@ import (
 	"errors"
 	"net/http"
 
-	"github.com/victorsteven/fullstack/api/auth"
-	"github.com/victorsteven/fullstack/api/responses"
+	"github.com/agushermanto22/go-gorm-jwt-postgres-mysql-and-testing/api/auth"
+	"github.com/agushermanto22/go-gorm-jwt-postgres-mysql-and-testing/api/responses"
 )
 
 func SetMiddlewareJSON(next http.HandlerFunc) http.HandlerFunc {
